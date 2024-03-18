beb414af316ad2fd1202f356ae78eee7bdc9546c
권예림 <124475716+yerim07@users.noreply.github.com>
2024-03-18 18:07:10 +0900

    test
    미리보기 이슈로 확인 불가하여, 일단 테스트 코드 커밋합니다!

---
+++ src/components/information/information.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const informationLayout = styled.div`
+  position: relative;
+
+  width: 720px;
+  height: 516px;
+  top: 282px;
+  left: 600px;
+  border-radius: 12px;
+  padding: 40px;
+  gap: 28px;
+
+  background: #fff;
+`;
